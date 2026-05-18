ceeccbb6ff794ce0cb7119fbda8f4bdb1502d7ff
bagus ryzen <bagusprasojo@tigaserangkai.co.id>
2026-05-18 11:25:22 +0700

    tambah static root

--- config/settings.py
+++ config/settings.py
@@ -72,6 +72,11 @@ USE_I18N = True
 USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "portal" / "static"]
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "/portal/login"
 
